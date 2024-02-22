de935aeed8cd0fa1eded1f94f200817aa2a2d1fe
marmanu <marcelo.mandirola@tarmac.io>
2024-02-21 21:56:08 -0300

    cleanlab of curated dataset

--- siamese_dataset.py
+++ siamese_dataset.py
@@ -1,8 +1,13 @@
-from validation import VALIDATION_DATASET
 from utils import load_video
 import hashlib
 import cv2
 from PIL import Image
+import csv
+from collections import defaultdict
+from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
+from numpy import asarray
+import json
+from functools import reduce
 
 
 def get_labels_by_hash(dataset):
@@ -25,4 +30,49 @@ def get_labels_by_hash(dataset):
     with open(f'siamese_tags.csv', 'w') as f:
         f.writelines(data)
 
-get_labels_by_hash(VALIDATION_DATASET)
+def reduce_intervals(acc, next):
+    if len(acc):
+        (_, end, interval, _) = acc[-1]
+        if next == interval:
+            acc[-1][1] += 1
+            return acc
+        else:
+            acc.append([end + 1, end + 1, next, ""])
+            return acc
+    else:
+        return [[1, 2, next, '']]
+
+
+    
+def stitch_frames_in_video(csv_file):
+    frames_by_video = defaultdict(list)
+    with open(csv_file, newline='\n') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(reader, None)
+        for path, tag, video, interval, video_index in reader:
+            frames_by_video[video].append((video_index, interval, tag, path))
+    train_labels = []
+    test_labels = []
+    all_labels = []
+    for video, data in frames_by_video.items():
+        sorted_data = sorted(data, key=lambda x: int(x[0]))
+        frames = list(map(lambda x: asarray(Image.open(f'curated/{x[3]}')), sorted_data))
+        labels = list(map(lambda x: x[1], sorted_data))[:len(frames) -1]
+        resumed_labels = reduce(reduce_intervals, labels, [])
+        print(resumed_labels)
+        clip = ImageSequenceClip(frames, fps=30)
+        clip.write_videofile(video)
+        all_labels.append((video, resumed_labels))
+        if data[0][2] == 'train':
+            train_labels.append((video, resumed_labels))
+        else:
+            test_labels.append((video, resumed_labels))
+    with open('curated_all.json', 'w') as f:
+        json.dump(dict(all_labels), f)
+    with open('curated_train.json', 'w') as f:
+        json.dump(dict(train_labels), f)
+    with open('curated_test.json', 'w') as f:
+        json.dump(dict(test_labels), f)
+    return dict(train_labels), dict(test_labels)
+
+stitch_frames_in_video('curated/labels.csv')
